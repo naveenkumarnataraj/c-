a1bf5b49b08740a5886cf7d479f04a90ee972047
naveenkumarnataraj <35687671+naveenkumarnataraj@users.noreply.github.com>
2018-02-02 16:41:02 +0530

    Create 9.C

---
+++ 9.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,k,sum=0;
+printf("enter the integers:");
+scanf("%d%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+printf("%d",i);
+}
+for(j=1;j<=k;j++)
+{
+sum=sum+j;
+}
+printf("the sum of %d integers:",sum);
+return 0;
+}                                                                                                                 );
